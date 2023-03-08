6f493a2bcae5ef14e873c8b307f99530db6b7715
ishanjain126 <32188592+ishanjain126@users.noreply.github.com>
2023-03-08 16:43:19 +0530

    Time: 39 ms (88.32%), Space: 13.9 MB (62.17%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,18 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        idx1 = None
+        pivot_idx = None
+        # Find the pivot index such that the array behaviour changes from increasing to decreasing 
         for i in range(len(nums) - 1, 0, -1):
             if nums[i-1] < nums[i]:
-                idx1 = i-1
+                pivot_idx = i-1
                 break
-
-        if idx1 is not None:
-            for i in range(len(nums) - 1, idx1, -1):
-                if(nums[i] > nums[idx1]):
-                    nums[idx1], nums[i] = nums[i], nums[idx1]
+                
+        # If pivot index exists execute the below logic else simply reverse the array to get the 
+        # very first permutation
+        
+        if pivot_idx is not None:
+            # Find an array element which is greater than the element at the pivot index and swap
+            for i in range(len(nums) - 1, pivot_idx, -1):
+                if(nums[i] > nums[pivot_idx]):
+                    nums[pivot_idx], nums[i] = nums[i], nums[pivot_idx]
                     break
-            nums[idx1+1:] = reversed(nums[idx1+1:])
+            # Reverse the array after the pivot index to the end
+            nums[pivot_idx+1:] = reversed(nums[pivot_idx+1:])
         else:
             nums.reverse()
 
