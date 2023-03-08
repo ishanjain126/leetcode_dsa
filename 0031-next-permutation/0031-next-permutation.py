8d666757dfd541c76803d89665e9ce5bbcf1af4f
ishanjain126 <32188592+ishanjain126@users.noreply.github.com>
2023-03-08 16:39:36 +0530

    Time: 50 ms (24.15%), Space: 13.8 MB (98.02%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx1 = None
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i-1] < nums[i]:
+                idx1 = i-1
+                break
+
+        if idx1 is not None:
+            for i in range(len(nums) - 1, idx1, -1):
+                if(nums[i] > nums[idx1]):
+                    nums[idx1], nums[i] = nums[i], nums[idx1]
+                    break
+            # j = -1
+            # for i in range((len(nums) // 2)):
+            #     nums[j], nums[i+idx1+1] = nums[i+idx1+1], nums[j]
+            #     j -= i
+            nums[idx1+1:] = reversed(nums[idx1+1:])
+        else:
+            nums.reverse()
+
+                
+        
