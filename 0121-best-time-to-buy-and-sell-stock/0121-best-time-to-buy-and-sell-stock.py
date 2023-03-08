a1b77837e4006db5d0eee0cc8c838eb2fdaa5b20
ishanjain126 <32188592+ishanjain126@users.noreply.github.com>
2023-03-09 00:47:27 +0530

    Time: 955 ms (90.67%), Space: 25 MB (31.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -12,23 +12,29 @@ class Solution:
         # return max_profit
         
         # Optimal approach
-        i = 0
-        j = 1
+        #         i = 0
+        #         j = 1
+        #         max_profit = 0
+        #         while j < len(prices):
+
+        #             min_ele = prices[i]
+        #             max_ele = prices[j]
+
+        #             if min_ele > max_ele:
+        #                 i = j
+        #             else:
+        #                 if max_ele - min_ele > max_profit:
+        #                     max_profit = max_ele - min_ele
+
+        #             j += 1
+
+        min_ele = math.inf
         max_profit = 0
-        while j < len(prices):
-                
-            min_ele = prices[i]
-            max_ele = prices[j]
-
-            if min_ele > max_ele:
-                i = j
-            else:
-                if max_ele - min_ele > max_profit:
-                    max_profit = max_ele - min_ele
-
-            j += 1
-            
-            
+        for i in range(len(prices)):
+            if prices[i] < min_ele:
+                min_ele = prices[i]
+            if (prices[i] - min_ele) > max_profit:
+                max_profit = prices[i] - min_ele
 
         return max_profit
                     
